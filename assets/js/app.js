9cf35fc0547d367825d42e814ae5e159dbd8f6e5
Diego Alencar Jacober <57761933898>
2023-05-26 16:21:21 -0300

    Finalizando design das telas

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,10 @@ $(document).ready(function() {
 
     $.get( "../../views/components/Menu/index.html", function( data ) {
         $( "header" ).html( data );
+    });
+
+      $.get( "../../views/components/Footer/index.html", function( data ) {
+        $( "footer" ).html( data );
       });
 
    
