a03a14d2ddddc81dab8af8665d35f11743d04d61
Diego Alencar Jacober <57761933898>
2023-05-26 16:55:24 -0300

    Pegando os produtos dinamicamente de um json

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,46 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-    $.get( "../../views/components/Menu/index.html", function( data ) {
-        $( "header" ).html( data );
-    });
+  $.get("../../views/components/Menu/index.html", function (data) {
+    $("header").html(data);
+  });
 
-      $.get( "../../views/components/Footer/index.html", function( data ) {
-        $( "footer" ).html( data );
-      });
+  $.get("../../views/components/Footer/index.html", function (data) {
+    $("footer").html(data);
+  });
 
-   
- });
+  });
+
+
+let carrinho = {
+  itens: [],
+
+  valorTotal:0
+}
+
+  console.log(produtos)
+  produtos.forEach(produto => {
+    console.log(produto)
+    div = `<div class="c-carousel__slide">`+
+    `<div class="produto">`+
+    `<div class="head--produto">`+
+        `<img src="${produto.url_imagem}" alt="" class="img--produto">`+
+    `</div>`+
+    `<div class="footer-produto">`+
+        `<div class="nome--ano">`+
+            `<p>${produto.nome}</p>`+
+            `<span><i class="fa-solid fa-calendar-days orange"></i> 2023</span>`+
+        ` </div>`+
+        `<div class="valor--produto">`+
+            `<span>${produto.valor}</span>`+
+        `</div>`+
+        `<div class="icones">`+
+            `<span><img src="./assets/imgs/icongas.svg" alt="" srcset=""> Gasolina</span>`+
+            `<span><img src="./assets/imgs/iconcambio.svg" alt="" srcset=""> Automático</span>`+
+        `</div>`+
+    `</div>`+
+    `</div>`+
+    `</div>`
+    document.getElementById('carros-destaques').innerHTML += div
+ 
+
+});
---
+++ assets/js/produtos.js
@@ -0,0 +1,24 @@
+let produtos = [
+    {
+        nome: "DIEGO X6",
+        marca: "BMW",
+        categoria: "esportivo",
+        valor: 229000.00,
+        cambio: "Automático",
+        combustivel: "Gasolina",
+        ano: 2023,
+        descricao: "4.4 M 4X4 COUPÉ V8 32V BI-TURBO GASOLINA 4P AUTOMÁTICO",
+        url_imagem: "./assets/imgs/bmw.svg"
+    },
+    {
+        nome: "BMW X1",
+        marca: "BMW",
+        categoria: "esportivo",
+        valor: 200000.00,
+        cambio: "Automático",
+        combustivel: "Gasolina",
+        ano: 2023,
+        descricao: "4.4 M 4X4 COUPÉ V8 32V BI-TURBO GASOLINA 4P AUTOMÁTICO",
+        url_imagem: "./assets/imgs/bmw.svg"
+    }
+]
